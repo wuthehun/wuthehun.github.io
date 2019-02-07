80b67d113b83535fa1bb784d1ba69eb203c75250
wuthehun <33743102+wuthehun@users.noreply.github.com>
2019-02-07 14:09:17 -0800

    color to page

---
+++ colors/precache-manifest.07b83f08b5e82cc8f71716e4ab4d1c94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8714162ad126aca26e02",
+    "url": "/static/js/main.8714162a.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "8714162ad126aca26e02",
+    "url": "/static/css/main.e0e7d87d.chunk.css"
+  },
+  {
+    "revision": "040de83e2e6611d1f8ab1d812e8c9907",
+    "url": "/index.html"
+  }
+];
